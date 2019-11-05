415ebb40105ac79b2d75d3e13401ad78e873f9aa
adim8 <56518601+adim8@users.noreply.github.com>
2019-11-05 20:29:07 +0200

    Conditional Statements - Lab

---
+++ L07-ConditionalStatementsLab/PasswordGuess.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PasswordGuess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = "s3cr3t!P@ssw0rd";
+        if (a.equals(b)) {
+            System.out.println("Welcome");
+        } else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
